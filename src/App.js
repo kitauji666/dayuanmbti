86fcaccf44303660e9391394ffbe9b61745212c9
kitauji666 <kitauji666@proton.me>
2023-01-08 14:25:39 -0500

    fix the routing issue

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/bgm/:id" element={<BangumiSummary/>} />
-        <Route path="/mbti" element={<MBTI/>} />
-        <Route path="/mbti/:id" element={<MBTIGroup/>} />
-        <Route path="/match/:id" element={<MatchPage/>} />
+        <Route path="/dayuanmbti" element={<Home/>} />
+        <Route path="/dayuanmbti/bgm/:id" element={<BangumiSummary/>} />
+        <Route path="/dayuanmbti/mbti" element={<MBTI/>} />
+        <Route path="/dayuanmbti/mbti/:id" element={<MBTIGroup/>} />
+        <Route path="/dayuanmbti/match/:id" element={<MatchPage/>} />
       </Routes>
     </BrowserRouter>
   );
